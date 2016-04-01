baf09b726bfe81ce3d6aa3d1db2e467578a58146
AbsoluteZero273 <chaudharypratik97@gmail.com>
2016-04-01 09:14:11 +0545

    intial commit

---
+++ js/base.js
@@ -0,0 +1,22 @@
+//scroll down to first content clicking bouncing arrow
+$('.bottom-btn').click(function() {
+ $('html, body').animate({scrollTop: $("#first-content").offset().top - 80}, 500);
+});
+
+//change navigation bar and brand logo with scroll
+var off = $('#first-content').offset().top;
+$(window).scroll(function() {
+  if($(window).scrollTop() >= off - 300){
+    $('.navbar-fixed-top').addClass('navbar-afterscroll');
+    $('.navbar-brand').addClass('navbar-brand-afterscroll');
+  }else{
+    $('.navbar-fixed-top').removeClass('navbar-afterscroll');
+    $('.navbar-brand').removeClass('navbar-brand-afterscroll');
+  }
+});
+
+//change navbar when nav button clicked
+$('.navbar-toggle').click(function(){
+  console.log("A");
+  $('.navbar, .navbar-collapse').addClass('navbar-collapse-aftertoggle');
+});
